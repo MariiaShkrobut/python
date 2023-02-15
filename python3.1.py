5daf2e1f240fd2611608f09c3b0292312b19a6b2
MariiaShkrobut <123626374+MariiaShkrobut@users.noreply.github.com>
2023-02-15 18:29:24 +0200

    Restaurant

---
+++ python3.1.py
@@ -0,0 +1,5 @@
+from random import randint
+randint(40,1200)
+menu = input ("Welcome to the cafe 'Monti'! Please, type what you would like to order using comma \n")
+order = menu.split(',')
+print("...\n" f'All together............{randint(40,1200)}')
