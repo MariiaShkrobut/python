b711fd582ea61b2412757ff65f49a59753c2e988
MariiaShkrobut <123626374+MariiaShkrobut@users.noreply.github.com>
2023-02-20 18:33:13 +0200

    modified

--- python3.1.py
+++ python3.1.py
@@ -1,5 +1,12 @@
-from random import randint
-randint(40,1200)
-menu = input ("Welcome to the cafe 'Monti'! Please, type what you would like to order using comma \n")
-order = menu.split(',')
-print("...\n" f'All together............{randint(40,1200)}')
+import random
+menu = input("Welcome to the cafe 'Monti'! Please, type what you would like to order using comma \n").split(", ")
+
+print('-------"Monti"-------')
+
+total_price = 0
+for item in menu:
+    price = random.randint(40, 1200)
+    total_price += price
+    print(f'{item}............{price} hrn')
+
+print(f'Total price: {total_price} hrn')
